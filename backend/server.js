6bbf9e6dcbd2884a52ff53394e434cee5cf8888a
vinusan velautham <vinusan_velautham@hotmail.com>
2023-03-28 12:16:03 -0400

    inital commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const express = require("express");
+const morgan = require("morgan");
+
+// IMPORT HANDLERS
+
+const PORT = 4000;
+
+express()
+	.use(function (req, res, next) {
+		res.header(
+			"Access-Control-Allow-Methods",
+			"OPTIONS, HEAD, GET, PUT, POST, DELETE"
+		);
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept"
+		);
+		next();
+	})
+	.use(morgan("tiny"))
+	.use(express.static("./server/assets"))
+	.use(express.json())
+	.use(express.urlencoded({ extended: false }))
+	.use("/", express.static(__dirname + "/"))
+
+	//Error message
+	.get("*", (req, res) => {
+		res.status(404).json({
+			status: 404,
+			message: "Error",
+		});
+	})
+
+	.listen(PORT, () => console.info(`Listening on port ${PORT}`));

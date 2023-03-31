7104695c9688e22d7ad950068bd15340c6ef3c0a
vinusan velautham <vinusan_velautham@hotmail.com>
2023-03-31 17:47:16 -0400

    logibtn error

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -3,19 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Homepage";
 import Header from "./Header";
 import Favorites from "./Favorites";
-import SignIn from "./SignIn";
+// import SignIn from "./SignIn";
 import styled from "styled-components";
 import LoginBtn from "./LoginBtn";
 
 function App() {
 	return (
 		<Router>
-			<HeadWrap>
-				<Header />
-				<LoginBtn />
-			</HeadWrap>
+			{/* <HeadWrap> */}
+			<Header />
+
+			{/* </HeadWrap> */}
 
 			<Routes>
+				<LoginBtn />
 				<Route path="/" element={<HomePage />} />
 				<Route path="/favorites" element={<Favorites />} />
 				{/* <Route path="/signin" element={<SignIn />} /> */}
@@ -26,7 +27,7 @@ function App() {
 
 export default App;
 
-const HeadWrap = styled.div`
-	display: flex;
-	justify-content: space-between;
-`;
+// const HeadWrap = styled.div`
+// 	display: flex;
+// 	justify-content: space-between;
+// `;

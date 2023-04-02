8c383f1f2d0827eb42ec351b104d7ea8ba80ebc9
vinusan velautham <vinusan_velautham@hotmail.com>
2023-04-02 11:13:19 -0400

    api fetchs functional

--- frontend/src/Components/MovieDetails.js
+++ frontend/src/Components/MovieDetails.js
@@ -6,9 +6,26 @@ import { useEffect } from "react";
 
 const MovieDetails = () => {
 	const { movieId } = useParams();
-
 	console.log(movieId);
-	return <MainCont>movie details</MainCont>;
+
+	const [movie, setMovie] = useState([]);
+
+	useEffect(() => {
+		fetch(
+			`https://api.themoviedb.org/3/movie/${movieId}?api_key=0552cedd4b9803ac1b9a12fe92f2d42b&language=en-US`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				setMovie(data);
+			})
+
+			.catch((error) => {
+				console.log(error);
+			});
+	}, [movieId]);
+
+	return <MainCont>movie details {movie.title}</MainCont>;
 };
 
 export default MovieDetails;

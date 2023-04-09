8751defedb2267bb73c67262e841ea494ea4f1d0
vinusan velautham <vinusan_velautham@hotmail.com>
2023-04-09 12:34:23 -0400

    movie details done

--- frontend/src/Components/MovieDetails.js
+++ frontend/src/Components/MovieDetails.js
@@ -29,9 +29,6 @@ const MovieDetails = () => {
 
 	return (
 		<MainCont>
-			<BgImg
-				src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-			/>
 			<SubCont>
 				<Img
 					src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
@@ -39,11 +36,13 @@ const MovieDetails = () => {
 				/>
 				<DetailsCont>
 					<Title>{movie.title}</Title>
-					<Tagline>-{movie.tagline}</Tagline>
-					<Info>{movie.release_date}</Info>
-					<Info>{movie.runtime} minutes </Info>
-					<Info>{roundedVoteAvg}/10</Info>
-					<Info>{movie.overview}</Info>
+					{movie.tagline && <Tagline>-{movie.tagline}</Tagline>}
+					<SubInfo>
+						<Info>Release Date: {movie.release_date}</Info>
+						<Info>Runtime: {movie.runtime} minutes </Info>
+						<Info>Avg Rating: {roundedVoteAvg}/10</Info>
+					</SubInfo>
+					<Overview>{movie.overview}</Overview>
 				</DetailsCont>
 			</SubCont>
 		</MainCont>
@@ -53,7 +52,7 @@ const MovieDetails = () => {
 export default MovieDetails;
 
 const MainCont = styled.div`
-	background-image: url(movie.);
+	background-image: linear-gradient(to top, darkred, white);
 `;
 
 const SubCont = styled.div`
@@ -61,38 +60,69 @@ const SubCont = styled.div`
 	/* justify-content: center; */
 `;
 const DetailsCont = styled.div`
-	color: white;
+	border-radius: 15px;
+
 	display: flex;
 	flex-direction: column;
 	justify-content: space-evenly;
-	background-color: rgba(0, 0, 0, 0.5);
-	height: 400px;
-	margin: 50px 0;
-	padding: 10px 25px;
+	/* background-color: rgba(0, 0, 0, 0.5); */
+	background-image: linear-gradient(to bottom, #284455, #3e6074);
+	/* height: 400px; */
+	margin: 35px 25px 35px 0;
+	padding: 0 5px;
 `;
 
 const Title = styled.div`
-	font-size: 40px;
 	align-self: center;
-	font-weight: 900;
+	font-weight: 100;
+
+	font-size: 25px;
+
+	padding: 15px;
+	border-radius: 5px;
+	font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+	background-color: darkred;
+	text-shadow: 0px 0px 15px white;
+	box-shadow: 0px 0px 5px white, 0px 0px 15px white;
+	color: white;
+	text-align: center;
 `;
 
-const Info = styled.div``;
+const Info = styled.div`
+	border-bottom: 2px solid white;
+	border-top: 2px solid white;
+	padding: 5px;
+	color: darkred;
+`;
+
+const SubInfo = styled.div`
+	font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+	display: flex;
+	align-items: center;
+	justify-content: space-evenly;
+	font-weight: 100;
+	font-size: 18px;
+	text-shadow: 0px 0px 15px white;
+`;
+
+const Overview = styled.div`
+	text-align: center;
+	font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+	font-size: 20px;
+	color: #edcba9;
+`;
 
 const Tagline = styled.div`
+	color: white;
 	font-style: italic;
+	text-align: center;
+	font-size: 20px;
+	font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
 `;
 
 const Img = styled.img`
-	height: 450px;
+	height: 460px;
 	margin: 25px 25px;
-`;
-
-const BgImg = styled.img`
-	/* height: 100vh; */
-	width: 98vw;
-	position: absolute;
-	z-index: -1;
-	opacity: 0.5;
-	filter: brightness(75%);
+	border-radius: 5px;
+	box-shadow: 0px 0px 5px white, 0px 0px 15px white;
 `;
